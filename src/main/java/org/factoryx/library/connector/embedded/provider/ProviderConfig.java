0ef054238b614b0b2ab77256b124d7c7bede6c38
Tobias Urbanek <tobias.urbanek@isst.fraunhofer.de>
2025-05-28 11:35:45 +0200

    chore: remove unused import

--- src/main/java/org/factoryx/library/connector/embedded/provider/ProviderConfig.java
+++ src/main/java/org/factoryx/library/connector/embedded/provider/ProviderConfig.java
@@ -20,13 +20,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
-//@EnableMongoRepositories(basePackages = "org.factoryx.library.connector.embedded.provider.repository")
 @ComponentScan(basePackages = "org.factoryx.library.connector.embedded.provider")
 /**
  * A bean providing config class
